3a9e138ff31b0b4c4f735a273c937ec95cb07815
yeongbinim <yeim.dev@gmail.com>
2024-12-20 20:17:57 +0900

    chore: 임시로 AuthController 뼈대 만들어둠

---
+++ src/main/java/sisibibi/wanttogram/controller/AuthController.java
@@ -0,0 +1,14 @@
+package sisibibi.wanttogram.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+	@RequestMapping("/login")
+	public ResponseEntity<Void> login() {
+		return ResponseEntity.ok().build();
+	}
+}
