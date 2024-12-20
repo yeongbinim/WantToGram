01f80aeebc235bb1da910e4330f19155c8a56632
yeongbinim <yeim.dev@gmail.com>
2024-12-20 17:49:02 +0900

    init: 프로젝트 환경 구성

---
+++ src/main/java/sisibibi/wanttogram/WantToGramApplication.java
@@ -0,0 +1,13 @@
+package sisibibi.wanttogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantToGramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantToGramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sisibibi/wanttogram/WantToGramApplicationTests.java
@@ -0,0 +1,13 @@
+package sisibibi.wanttogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantToGramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
