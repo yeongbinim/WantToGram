325eedf0304672c47547f9459d07003e3569945d
yeongbinim <yeim.dev@gmail.com>
2024-12-23 12:18:05 +0900

    refactor: Service에서 던지는 예외들 커스텀으로 변경

--- src/main/java/sisibibi/wanttogram/member/service/MemberService.java
+++ src/main/java/sisibibi/wanttogram/member/service/MemberService.java
@@ -3,6 +3,8 @@ package sisibibi.wanttogram.member.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sisibibi.wanttogram.common.PasswordEncoder;
+import sisibibi.wanttogram.common.exception.InvalidPasswordException;
+import sisibibi.wanttogram.common.exception.NotFoundException;
 import sisibibi.wanttogram.member.dto.MemberUpdate;
 import sisibibi.wanttogram.member.entity.MemberEntity;
 import sisibibi.wanttogram.member.infrastructure.MemberRepository;
@@ -15,15 +17,14 @@ public class MemberService {
 	private final PasswordEncoder passwordEncoder;
 
 	public MemberEntity getMember(Long id) {
-		// TODO Custom Exception 만들기
 		return memberRepository.findById(id)
-			.orElseThrow(() -> new IllegalArgumentException("멤버를 찾을 수 없음"));
+			.orElseThrow(() -> new NotFoundException("멤버", id));
 	}
 
 	public MemberEntity updateMember(Long id, MemberUpdate memberUpdate) {
 		MemberEntity member = getMember(id);
 		if (!passwordEncoder.matches(memberUpdate.getPassword(), member.getPassword())) {
-			throw new IllegalArgumentException("패스워드가 잘못됨");
+			throw new InvalidPasswordException();
 		}
 		return memberRepository.save(member.update(memberUpdate));
 	}
