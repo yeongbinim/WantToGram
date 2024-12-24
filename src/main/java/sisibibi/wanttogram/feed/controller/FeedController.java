a88d114788c72e71a58d2a0daff7c3076b1c900c
answerin1 <dabin990723@naver.com>
2024-12-24 10:35:42 +0900

    feat: feed 조회 기능 구현 추가

--- src/main/java/sisibibi/wanttogram/feed/controller/FeedController.java
+++ src/main/java/sisibibi/wanttogram/feed/controller/FeedController.java
@@ -27,12 +27,20 @@ public class FeedController {
         return ResponseEntity.status(HttpStatus.CREATED).body(feedResponseDto);
     }
 
-    @GetMapping // 피드 전체 조회 -> 페이징 조회
+    @GetMapping // 피드 전체 조회 -> 페이징 조회 수정 예정
     public ResponseEntity<List<FeedResponseDto>> findAllFeed() {
         List<FeedResponseDto> feedResponseDtosList = feedService.findAllFeed();
         return new ResponseEntity<>(feedResponseDtosList, HttpStatus.OK);
     }
 
+    // 피드 단일 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<FeedResponseDto> findFeedById(@PathVariable Long id) {
+        FeedResponseDto feedResponseDto = feedService.findFeedById(id);
+        return new ResponseEntity<>(feedResponseDto, HttpStatus.OK);
+    }
+
+    // 피드 수정
     @PutMapping("/{id}")
     public ResponseEntity<FeedResponseDto> updateFeed(@PathVariable Long id, @RequestBody FeedRequestDto request) {
         FeedEntity feed = feedService.updateFeed(id, request);
@@ -41,6 +49,7 @@ public class FeedController {
         return new ResponseEntity<>(feedResponseDto, HttpStatus.OK);
     }
 
+    // 피드 삭제
     @DeleteMapping("/{id}") // 피드 삭제
     public ResponseEntity<FeedResponseDto> deleteFeed(@PathVariable Long id) {
         feedService.deleteFeed(id);
--- src/main/java/sisibibi/wanttogram/feed/service/FeedService.java
+++ src/main/java/sisibibi/wanttogram/feed/service/FeedService.java
@@ -3,9 +3,13 @@ package sisibibi.wanttogram.feed.service;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import sisibibi.wanttogram.feed.domain.FeedResponseDto;
 import sisibibi.wanttogram.feed.domain.FeedRequestDto;
 import sisibibi.wanttogram.feed.entity.FeedEntity;
@@ -25,6 +29,7 @@ public class FeedService {
     private final HttpSession session;
     private final MemberRepository memberRepository;
 
+
     public FeedEntity createFeed(FeedRequestDto request) {
         MemberEntity member = memberRepository.findByEmail((String) session.getAttribute("userEmail"))
                 .orElseThrow(() -> new RuntimeException("User not found"));
@@ -33,6 +38,7 @@ public class FeedService {
 
     }
 
+    // 피드 페이징 조회 (전제 조회 까지 구현)
     @Transactional(readOnly = true)
     public List<FeedResponseDto> findAllFeed() {
         List<FeedEntity> feedList = feedRepository.findAll();
@@ -41,6 +47,12 @@ public class FeedService {
                 .map(feedEntity -> new FeedResponseDto(feedEntity.getWriter().getName(), feedEntity.getTitle(), feedEntity.getContent(), feedEntity.getCreatedAt(), feedEntity.getUpdatedAt()))
                 .toList();
         return dtoList;
+    }
+
+    // 피드 단일 조회
+    public FeedResponseDto findFeedById(Long id) {
+        FeedEntity feed = feedRepository.findFeedByIdOrElseThrowById(id);
+        return new FeedResponseDto(feed.getWriter().getName(), feed.getTitle(), feed.getContent(), feed.getCreatedAt(), feed.getUpdatedAt());
     }
 
 
