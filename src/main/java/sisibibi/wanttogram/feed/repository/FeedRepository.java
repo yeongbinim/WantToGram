52f3465515b09c4c8645215090babc0c75d7c6bf
yeongbinim <yeim.dev@gmail.com>
2024-12-26 10:08:19 +0900

    refactor: FeedRepository JPQL 쿼리문 가독성 있도록 변경

--- src/main/java/sisibibi/wanttogram/feed/repository/FeedRepository.java
+++ src/main/java/sisibibi/wanttogram/feed/repository/FeedRepository.java
@@ -1,5 +1,6 @@
 package sisibibi.wanttogram.feed.repository;
 
+import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,19 +10,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 import sisibibi.wanttogram.feed.entity.FeedEntity;
 
-import java.time.LocalDateTime;
-
 public interface FeedRepository extends JpaRepository<FeedEntity, Long> {
 
-    default FeedEntity findFeedByIdOrElseThrowById(Long id) {
-        return findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Feed not found"));
-    }
+	default FeedEntity findFeedByIdOrElseThrowById(Long id) {
+		return findById(id).orElseThrow(
+			() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Feed not found"));
+	}
 
-    Page<FeedEntity> findAllByUpdatedAtBetween(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
+	Page<FeedEntity> findAllByUpdatedAtBetween(LocalDateTime startDate, LocalDateTime endDate,
+		Pageable pageable);
 
-    @Query("SELECT f FROM FeedEntity f WHERE f.writer.id IN (SELECT fl.following.id FROM FollowEntity fl WHERE fl.follower.id = :userId) " +
-            "OR f.writer.id = :userId")
-    Page<FeedEntity> findAllFeedsByFollowing(@Param("userId") Long userId, Pageable pageable);
+	@Query("""
+		SELECT f
+		FROM FeedEntity f
+		WHERE f.writer.id IN (
+		  SELECT fl.following.id
+		  FROM FollowEntity fl
+		  WHERE fl.follower.id = :userId)
+		OR f.writer.id = :userId
+		""")
+	Page<FeedEntity> findAllFeedsByFollowing(@Param("userId") Long userId, Pageable pageable);
 
 
 }
