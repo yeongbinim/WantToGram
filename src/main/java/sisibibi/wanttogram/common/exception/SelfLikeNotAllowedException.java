cb0d9236d3c6655b9c28c4bc616361dda2f084dd
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2024-12-26 10:52:06 +0900

    Refactor: 본인의 피드, 댓글에는 좋아요 불가능 하도록 수정

---
+++ src/main/java/sisibibi/wanttogram/common/exception/SelfLikeNotAllowedException.java
@@ -0,0 +1,12 @@
+package sisibibi.wanttogram.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.server.ResponseStatusException;
+
+public class SelfLikeNotAllowedException extends ResponseStatusException {
+
+    public SelfLikeNotAllowedException(String message) {
+        super(HttpStatus.BAD_REQUEST,message);
+    }
+}
