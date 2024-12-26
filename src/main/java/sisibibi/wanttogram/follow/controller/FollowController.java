853b36d4c18fc9da3da26c683ebe4256e18d6ffc
yeongbinim <yeim.dev@gmail.com>
2024-12-26 21:00:17 +0900

    refactor: api 명세대로 변경

--- src/main/java/sisibibi/wanttogram/follow/controller/FollowController.java
+++ src/main/java/sisibibi/wanttogram/follow/controller/FollowController.java
@@ -1,69 +1,77 @@
 package sisibibi.wanttogram.follow.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import sisibibi.wanttogram.follow.dto.FollowerListResponseDto;
-import sisibibi.wanttogram.follow.dto.FollowingListResponseDto;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import sisibibi.wanttogram.follow.dto.FollowRequestDto;
 import sisibibi.wanttogram.follow.dto.FollowResponseDto;
+import sisibibi.wanttogram.follow.dto.FollowerListResponseDto;
+import sisibibi.wanttogram.follow.dto.FollowingListResponseDto;
 import sisibibi.wanttogram.follow.service.FollowService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
 public class FollowController {
 
-    // 속성
-    private final FollowService followService;
+	// 속성
+	private final FollowService followService;
 
+	// 생성자
 
-    // 생성자
+	// 기능
+	// ::: 팔로우 추가 API
+	@PostMapping("/{member_id}/follows")
+	public ResponseEntity<FollowResponseDto> saveFollow(
+		@PathVariable Long member_id,
+		@RequestBody FollowRequestDto requestDto
+	) {
 
-    // 기능
-    // ::: 팔로우 추가 API
-    @PostMapping("/{member_id}/follows")
-    public ResponseEntity<FollowResponseDto> saveFollow(
-            @PathVariable Long member_id,
-            @RequestBody FollowRequestDto requestDto
-            ) {
+		FollowResponseDto savedFollow = followService.save(member_id, requestDto);
 
-        FollowResponseDto savedFollow = followService.save(member_id, requestDto);
+		return new ResponseEntity<>(savedFollow, HttpStatus.CREATED);
+	}
 
-        return new ResponseEntity<>(savedFollow, HttpStatus.CREATED);
-    }
+	// ::: 특정 멤버의 팔로우 전체 조회 API
+	@GetMapping("/{member_id}/follows/followers")
+	public ResponseEntity<List<FollowingListResponseDto>> findAllByFollowingId(
+		@PathVariable Long member_id) {
 
-    // ::: 특정 멤버의 팔로우 전체 조회 API
-    @GetMapping("/{member_id}/follows/followings")
-    public ResponseEntity<List<FollowingListResponseDto>> findAllByFollowingId(@PathVariable Long member_id) {
+		List<FollowingListResponseDto> followingListResponseDtoList = followService.findAllByFollowingId(
+			member_id);
 
-        List<FollowingListResponseDto> followingListResponseDtoList = followService.findAllByFollowingId(member_id);
+		return new ResponseEntity<>(followingListResponseDtoList, HttpStatus.OK);
+	}
 
-        return new ResponseEntity<>(followingListResponseDtoList, HttpStatus.OK);
-    }
+	// ::: 팔로워 전체 조회 API
+	@GetMapping("/{member_id}/follows/followings")
+	public ResponseEntity<List<FollowerListResponseDto>> findAllByFollowerId(
+		@PathVariable Long member_id) {
 
-    // ::: 팔로워 전체 조회 API
-    @GetMapping("/{member_id}/follows/followers")
-    public ResponseEntity<List<FollowerListResponseDto>> findAllByFollowerId(@PathVariable Long member_id) {
+		List<FollowerListResponseDto> followerListResponseDtoList = followService.findAllByFollowerId(
+			member_id);
 
-        List<FollowerListResponseDto> followerListResponseDtoList = followService.findAllByFollowerId(member_id);
+		return new ResponseEntity<>(followerListResponseDtoList, HttpStatus.OK);
+	}
 
-        return new ResponseEntity<>(followerListResponseDtoList, HttpStatus.OK);
-    }
+	// ::: 팔로우 삭제 API
+	@DeleteMapping("/{member_id}/follows/{id}")
+	public ResponseEntity<Void> deleteFollow(
+		@PathVariable Long member_id,
+		@PathVariable Long id
+	) {
 
-    // ::: 팔로우 삭제 API
-    @DeleteMapping("/{member_id}/follows/{id}")
-    public ResponseEntity<Void> deleteFollow(
-            @PathVariable Long member_id,
-            @PathVariable Long id
-    ) {
+		followService.delete(member_id, id);
 
-        followService.delete(member_id, id);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
 }
--- src/main/java/sisibibi/wanttogram/like/controller/LikeController.java
+++ src/main/java/sisibibi/wanttogram/like/controller/LikeController.java
@@ -3,34 +3,41 @@ package sisibibi.wanttogram.like.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import sisibibi.wanttogram.like.service.LikeService;
 
 @RestController
 @RequestMapping("feeds")
 @RequiredArgsConstructor
 public class LikeController {
-    private final LikeService likeService;
 
-    @GetMapping("/{feedId}/likes")
-    public ResponseEntity<Void> feedLike (@PathVariable Long feedId){
-        likeService.feedlike(feedId);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-    @DeleteMapping("/{feedId}/likes")
-    public ResponseEntity<Void> feedUnLike (@PathVariable Long feedId){
-        likeService.feedUnlike(feedId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+	private final LikeService likeService;
 
-    @GetMapping("/comments/{comments_id}/likes")
-    public ResponseEntity<Void> commentLike (@PathVariable Long comments_id){
-        likeService.commentLike(comments_id);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-    @DeleteMapping("/comments/{comments_id}/likes")
-    public ResponseEntity<Void> commentUnLike (@PathVariable Long comments_id){
-        likeService.commentUnLike(comments_id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+	@PostMapping("/{feedId}/likes")
+	public ResponseEntity<Void> feedLike(@PathVariable Long feedId) {
+		likeService.feedlike(feedId);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/{feedId}/likes")
+	public ResponseEntity<Void> feedUnLike(@PathVariable Long feedId) {
+		likeService.feedUnlike(feedId);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@PostMapping("/comments/{comments_id}/likes")
+	public ResponseEntity<Void> commentLike(@PathVariable Long comments_id) {
+		likeService.commentLike(comments_id);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/comments/{comments_id}/likes")
+	public ResponseEntity<Void> commentUnLike(@PathVariable Long comments_id) {
+		likeService.commentUnLike(comments_id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
 }
